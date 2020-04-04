bc3a94530264bcf883e801b2a453d307f778fd70
Michael Wurster <miwurster@googlemail.com>
2020-04-04 09:53:26 +0200

    Init Material

--- tosca-lightning-app/src/App.tsx
+++ tosca-lightning-app/src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, CssBaseline } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CssBaseline>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </CssBaseline>
   );
 }
 
