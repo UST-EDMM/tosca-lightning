06427ae0d1d0260713e272aa76b876d257db2611
Michael Wurster <miwurster@googlemail.com>
2021-01-21 14:15:35 +0100

    Fix app config and update run config

--- tosca-lightning-app/src/config.ts
+++ tosca-lightning-app/src/config.ts
@@ -1,6 +1,1 @@
-export default {
-  backend: {
-    baseUrl: 'http://localhost:9000', // dev
-    // baseUrl: 'http://localhost:8080', // prod
-  },
-};
+export default {};
--- tosca-lightning-app/src/service/backend.ts
+++ tosca-lightning-app/src/service/backend.ts
@@ -1,28 +1,29 @@
-import config from '../config';
 import { fetchOrThrow } from './fetch';
 import { Config } from '../models/Config';
 import { Plugin } from '../models/Plugin';
 import { ServiceTemplate } from '../models/ServiceTemplate';
 import { TransformResponse } from '../models/TransformResponse';
 
+const baseUrl = window.location.protocol + "//" + window.location.hostname + ":9000";
+
 const getConfig = async (): Promise<Config> => {
-  const response = await fetchOrThrow(`${config.backend.baseUrl}/config`, { method: 'GET' });
+  const response = await fetchOrThrow(`${baseUrl}/config`, { method: 'GET' });
   return (await response.json()) as Config;
 };
 
 const getPlugins = async (): Promise<Plugin[]> => {
-  const response = await fetchOrThrow(`${config.backend.baseUrl}/plugins`, { method: 'GET' });
+  const response = await fetchOrThrow(`${baseUrl}/plugins`, { method: 'GET' });
   return (await response.json()) as Plugin[];
 };
 
 const getServiceTemplates = async (): Promise<ServiceTemplate[]> => {
-  const response = await fetchOrThrow(`${config.backend.baseUrl}/service-templates`, { method: 'GET' });
+  const response = await fetchOrThrow(`${baseUrl}/service-templates`, { method: 'GET' });
   return (await response.json()) as ServiceTemplate[];
 };
 
 const transformServiceTemplate = async (target: string, st: ServiceTemplate): Promise<TransformResponse> => {
   const response = await fetchOrThrow(
-    `${config.backend.baseUrl}/service-templates/${st.id}/transform/${target}`,
+    `${baseUrl}/service-templates/${st.id}/transform/${target}`,
     { method: 'POST' }
   );
   return (await response.json()) as TransformResponse;
